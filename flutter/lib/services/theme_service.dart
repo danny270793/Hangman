2ec75ec202cfdf4cc22c03f9bac939340cf26d6a
Danny Vaca <danny270793@icloud.com>
2026-01-06 15:03:11 -0500

    Refactor: format ThemeService code style

--- flutter/lib/services/theme_service.dart
+++ flutter/lib/services/theme_service.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-enum AppThemeMode {
-  system,
-  light,
-  dark,
-}
+enum AppThemeMode { system, light, dark }
 
 class ThemeService extends ChangeNotifier {
   static const String _themeKey = 'app_theme_mode';
@@ -29,7 +25,7 @@ class ThemeService extends ChangeNotifier {
   Future<void> loadTheme() async {
     final prefs = await SharedPreferences.getInstance();
     final themeString = prefs.getString(_themeKey);
-    
+
     if (themeString != null) {
       _themeMode = AppThemeMode.values.firstWhere(
         (mode) => mode.toString() == themeString,
@@ -42,14 +38,13 @@ class ThemeService extends ChangeNotifier {
   /// Set the theme and save to storage
   Future<void> setTheme(AppThemeMode mode) async {
     if (_themeMode == mode) return;
-    
+
     _themeMode = mode;
-    
+
     // Save to storage
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(_themeKey, mode.toString());
-    
+
     notifyListeners();
   }
 }
-
