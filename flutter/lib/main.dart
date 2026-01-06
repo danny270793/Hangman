8f4779d0891a0e5492c50d3eba0ce2b4af15e7d3
Danny Vaca <danny270793@icloud.com>
2026-01-06 08:45:47 -0500

    feat: enhance routing logic in MyApp to manage authentication state and redirect users accordingly

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -5,22 +5,51 @@ import 'package:go_router/go_router.dart';
 import 'package:hangman/pages/splash.dart';
 import 'package:hangman/pages/login.dart';
 import 'package:hangman/pages/home.dart';
+import 'package:hangman/services/auth_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final protectedPages = const [
+    HomePage.routeName,
+  ];
+  final publicPages = const [
+    SplashPage.routeName,
+    LoginPage.routeName,
+  ];
+  final authService = AuthService();
+
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: GoRouter(routes: [
-        GoRoute(path: SplashPage.routeName, builder: (context, state) => const SplashPage()),
-        GoRoute(path: LoginPage.routeName, builder: (context, state) => const LoginPage()),
-        GoRoute(path: HomePage.routeName, builder: (context, state) => const HomePage()),
-      ]),
+      routerConfig: GoRouter(
+        refreshListenable: authService,
+        redirect: (context, state) {
+          final isLoggedIn = authService.isAuthenticated;
+          final location = state.matchedLocation;
+
+          final isPublicPage = publicPages.contains(location);
+          final isProtectedPage = protectedPages.contains(location);
+
+          if (!isLoggedIn && isProtectedPage) {
+            return LoginPage.routeName;
+          }
+
+          if (isLoggedIn && isPublicPage) {
+            return HomePage.routeName;
+          }
+
+          return null;
+        },
+        routes: [
+          GoRoute(path: SplashPage.routeName, builder: (context, state) => const SplashPage()),
+          GoRoute(path: LoginPage.routeName, builder: (context, state) => const LoginPage()),
+          GoRoute(path: HomePage.routeName, builder: (context, state) => const HomePage()),
+        ]),
       title: 'Flutter Demo',
       localizationsDelegates: const [
         AppLocalizations.delegate,
