f493b7241e59acbec30513f74cd00e18e23bdfd1
Danny Vaca <danny270793@icloud.com>
2026-01-08 10:20:37 -0500

    Fix formatting in supabase_config.dart

--- flutter/lib/config/supabase_config.dart
+++ flutter/lib/config/supabase_config.dart
@@ -5,8 +5,7 @@ class SupabaseConfig {
   // Make sure to create a .env file with SUPABASE_URL and SUPABASE_ANON_KEY
   static String get supabaseUrl =>
       dotenv.env['SUPABASE_URL'] ?? 'YOUR_SUPABASE_URL';
-  
+
   static String get supabaseAnonKey =>
       dotenv.env['SUPABASE_ANON_KEY'] ?? 'YOUR_SUPABASE_ANON_KEY';
 }
-
