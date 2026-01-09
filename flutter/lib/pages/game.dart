efd4f88dcda200bdac4bb0df6d939ca77f28a606
Danny Vaca <danny270793@icloud.com>
2026-01-09 10:11:33 -0500

    Update keyboard layout in game.dart to include 'Ñ' for enhanced language support

--- flutter/lib/pages/game.dart
+++ flutter/lib/pages/game.dart
@@ -854,7 +854,7 @@ class _GamePageState extends State<GamePage> {
   Widget _buildKeyboard() {
     const letters = [
       ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-      ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+      ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ'],
       ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
     ];
 
